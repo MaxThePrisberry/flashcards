fde213979b4372d181b3fa13fc6bb159ba8c458a
ChillyOctopus <111397038+ChillyOctopus@users.noreply.github.com>
2026-03-09 16:24:42 -0600

    Rename History table to ReviewHistory.
    Moved 'score' column to 'Scores" table. Made timestamp UTC (timestampz vs timestamp) coordinated.

--- database/init/06_history.sql
+++ database/init/06_history.sql
@@ -1,8 +1,7 @@
-CREATE TABLE "History" (
+CREATE TABLE "ReviewHistory" (
     history_id SERIAL PRIMARY KEY,
     user_id    INT       NOT NULL REFERENCES "User"(user_id) ON DELETE CASCADE,
     pair_id    INT       NOT NULL REFERENCES "Pair"(pair_id) ON DELETE CASCADE,
-    score      INT       NOT NULL,
-    got_it     BOOLEAN   NOT NULL,
-    reviewed_at TIMESTAMP NOT NULL DEFAULT NOW()
+    got_it     BOOLEAN   NOT NULL DEFAULT FALSE,
+    reviewed_at TIMESTAMPZ NOT NULL DEFAULT NOW()
 );
