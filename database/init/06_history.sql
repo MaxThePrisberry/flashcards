eb5ac50ce376027143dd6340526fb9aca5f0e575
ChillyOctopus <111397038+ChillyOctopus@users.noreply.github.com>
2026-03-09 17:05:36 -0600

    Update ReviewHistory table structure
    Replaced pair_id with deck_id in ReviewHistory table.

--- database/init/06_history.sql
+++ database/init/06_history.sql
@@ -1,7 +1,6 @@
 CREATE TABLE "ReviewHistory" (
     history_id SERIAL PRIMARY KEY,
     user_id    INT       NOT NULL REFERENCES "User"(user_id) ON DELETE CASCADE,
-    pair_id    INT       NOT NULL REFERENCES "Pair"(pair_id) ON DELETE CASCADE,
-    got_it     BOOLEAN   NOT NULL DEFAULT FALSE,
+    deck_id    INT       NOT NULL REFERENCES "Deck"(deck_id) ON DELETE CASCADE,
     reviewed_at TIMESTAMPZ NOT NULL DEFAULT NOW()
 );
