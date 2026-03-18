a373e978d10b646bddcbf1fb732e1087c8da1653
ChillyOctopus <111397038+ChillyOctopus@users.noreply.github.com>
2026-03-18 10:08:07 -0600

    Change history_id from SERIAL to UUID

--- database/init/06_history.sql
+++ database/init/06_history.sql
@@ -1,5 +1,5 @@
 CREATE TABLE "ReviewHistory" (
-    history_id SERIAL PRIMARY KEY,
+    history_id UUID PRIMARY KEY,
     user_id    INT       NOT NULL REFERENCES "User"(user_id) ON DELETE CASCADE,
     deck_id    INT       NOT NULL REFERENCES "Deck"(deck_id) ON DELETE CASCADE,
     reviewed_at TIMESTAMPZ NOT NULL DEFAULT NOW()
