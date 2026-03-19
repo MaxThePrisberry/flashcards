089562c0c09dd9cfffc4879ccf08cd4a27deb61b
Landon Whitaker <landonbballwhitaker@gmail.com>
2026-03-18 23:16:03 -0600

    fixed tables and added api docs (#12)

--- database/init/06_history.sql
+++ database/init/06_history.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "ReviewHistory" (
     history_id UUID PRIMARY KEY,
-    user_id    INT       NOT NULL REFERENCES "User"(user_id) ON DELETE CASCADE,
-    deck_id    INT       NOT NULL REFERENCES "Deck"(deck_id) ON DELETE CASCADE,
+    user_id    UUID      NOT NULL REFERENCES "User"(user_id) ON DELETE CASCADE,
+    deck_id    UUID      NOT NULL REFERENCES "Deck"(deck_id) ON DELETE CASCADE,
     reviewed_at TIMESTAMPZ NOT NULL DEFAULT NOW()
 );
